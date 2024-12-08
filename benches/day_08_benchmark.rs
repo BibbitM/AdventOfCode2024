5fc4256c34bf5c5732781f4122ed05960ab538ae
Michał Nowak <bibbit@wp.pl>
2024-12-08 22:24:30 +0100

    Optimize the Day 08 solution
    Instead of `Vec<>.contain()`, add each found antinode to the vector and remove duplicates at the end (`sort()` + `dedup()`).
    Speedup for part one - x 5;
    Speedup for part two - x 10;
    
    Also, I've checked `HashSet<>.insert()` instead of vector. It was 2x slower than the optimized solution.

---
+++ benches/day_08_benchmark.rs
@@ -0,0 +1,39 @@
+﻿use advent::char_map::CharMap;
+use advent::day_08;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::fs;
+
+fn day_08_part_one_benchmark(c: &mut Criterion) {
+    let input = "inputs\\day_08.txt";
+    let input = fs::read_to_string(input).expect("Error: Failed to read file 'inputs\\day_08.txt'");
+
+    let antennas_map = CharMap::new(input.to_string());
+    let antennas = day_08::gather_antennas(&antennas_map);
+
+    let mut group = c.benchmark_group("day_08__part_one");
+
+    group.bench_function("original", |b| b.iter(|| _ = day_08::find_antinodes(&antennas, &antennas_map)));
+
+    group.bench_function("sort_dedup", |b| b.iter(|| _ = day_08::find_antinodes_sort_dedup(&antennas, &antennas_map)));
+
+    group.bench_function("hash_set", |b| b.iter(|| _ = day_08::find_antinodes_hash_set(&antennas, &antennas_map)));
+}
+
+fn day_08_part_two_benchmark_part_two(c: &mut Criterion) {
+    let input = "inputs\\day_08.txt";
+    let input = fs::read_to_string(input).expect("Error: Failed to read file 'inputs\\day_08.txt'");
+
+    let antennas_map = CharMap::new(input.to_string());
+    let antennas = day_08::gather_antennas(&antennas_map);
+
+    let mut group = c.benchmark_group("day_08__part_two");
+
+    group.bench_function("original", |b| b.iter(|| _ = day_08::find_antinodes_in_line(&antennas, &antennas_map)));
+
+    group.bench_function("sort_dedup", |b| b.iter(|| _ = day_08::find_antinodes_in_line_sort_dedup(&antennas, &antennas_map)));
+
+    group.bench_function("hash_set", |b| b.iter(|| _ = day_08::find_antinodes_in_line_hash_set(&antennas, &antennas_map)));
+}
+
+criterion_group!(benches, day_08_part_one_benchmark, day_08_part_two_benchmark_part_two);
+criterion_main!(benches);
--- lib/day_08.rs
+++ lib/day_08.rs
@@ -1,4 +1,5 @@
 ﻿use crate::char_map::CharMap;
+use std::collections::HashSet;
 
 pub fn gather_antennas(antennas_map: &CharMap) -> std::collections::HashMap<char, Vec<(i32, i32)>> {
     let mut antennas: std::collections::HashMap<char, Vec<(i32, i32)>> = std::collections::HashMap::new();
@@ -19,6 +20,7 @@ pub fn gather_antennas(antennas_map: &CharMap) -> std::collections::HashMap<char
 pub fn find_antinodes(antennas: &std::collections::HashMap<char, Vec<(i32, i32)>>, antennas_map: &CharMap) -> Vec<(i32, i32)> {
     let mut antinodes = Vec::new();
 
+    #[inline]
     fn is_in_bounds(pos: &(i32, i32), antennas_map: &CharMap) -> bool {
         return pos.0 >= 0 && pos.0 < antennas_map.width && pos.1 >= 0 && pos.1 < antennas_map.height;
     }
@@ -47,13 +49,73 @@ pub fn find_antinodes(antennas: &std::collections::HashMap<char, Vec<(i32, i32)>
     return antinodes;
 }
 
+pub fn find_antinodes_sort_dedup(antennas: &std::collections::HashMap<char, Vec<(i32, i32)>>, antennas_map: &CharMap) -> Vec<(i32, i32)> {
+    let mut antinodes = Vec::new();
+
+    #[inline]
+    fn is_in_bounds(pos: &(i32, i32), antennas_map: &CharMap) -> bool {
+        return pos.0 >= 0 && pos.0 < antennas_map.width && pos.1 >= 0 && pos.1 < antennas_map.height;
+    }
+
+    for antenna in antennas.values() {
+        for i in 0..antenna.len() - 1 {
+            for j in i + 1..antenna.len() {
+                let antenna1 = antenna[i];
+                let antenna2 = antenna[j];
+
+                let offset = (antenna2.0 - antenna1.0, antenna2.1 - antenna1.1);
+
+                let antinode1 = (antenna1.0 - offset.0, antenna1.1 - offset.1);
+                let antinode2 = (antenna2.0 + offset.0, antenna2.1 + offset.1);
+
+                if is_in_bounds(&antinode1, &antennas_map) {
+                    antinodes.push(antinode1);
+                }
+                if is_in_bounds(&antinode2, &antennas_map) {
+                    antinodes.push(antinode2);
+                }
+            }
+        }
+    }
+
+    antinodes.sort();
+    antinodes.dedup();
+    return antinodes;
+}
+
+pub fn find_antinodes_hash_set(antennas: &std::collections::HashMap<char, Vec<(i32, i32)>>, antennas_map: &CharMap) -> Vec<(i32, i32)> {
+    let mut antinodes = HashSet::new();
+
+    #[inline]
+    fn is_in_bounds(pos: &(i32, i32), antennas_map: &CharMap) -> bool {
+        return pos.0 >= 0 && pos.0 < antennas_map.width && pos.1 >= 0 && pos.1 < antennas_map.height;
+    }
+
+    for antenna in antennas.values() {
+        for i in 0..antenna.len() - 1 {
+            for j in i + 1..antenna.len() {
+                let antenna1 = antenna[i];
+                let antenna2 = antenna[j];
+
+                let offset = (antenna2.0 - antenna1.0, antenna2.1 - antenna1.1);
+
+                let antinode1 = (antenna1.0 - offset.0, antenna1.1 - offset.1);
+                let antinode2 = (antenna2.0 + offset.0, antenna2.1 + offset.1);
+
+                if is_in_bounds(&antinode1, &antennas_map) {
+                    antinodes.insert(antinode1);
+                }
+                if is_in_bounds(&antinode2, &antennas_map) {
+                    antinodes.insert(antinode2);
+                }
+            }
+        }
+    }
+
+    return antinodes.iter().copied().collect();
+}
+
 fn gcd(mut a: i32, mut b: i32) -> i32 {
-    if a == 0 {
-        return b.abs();
-    }
-    if b == 0 {
-        return a.abs();
-    }
     while b != 0 {
         let temp = b;
         b = a % b;
@@ -61,9 +123,11 @@ fn gcd(mut a: i32, mut b: i32) -> i32 {
     }
     return a.abs();
 }
+
 pub fn find_antinodes_in_line(antennas: &std::collections::HashMap<char, Vec<(i32, i32)>>, antennas_map: &CharMap) -> Vec<(i32, i32)> {
     let mut antinodes = Vec::new();
 
+    #[inline]
     fn is_in_bounds(pos: &(i32, i32), antennas_map: &CharMap) -> bool {
         return pos.0 >= 0 && pos.0 < antennas_map.width && pos.1 >= 0 && pos.1 < antennas_map.height;
     }
@@ -122,6 +186,112 @@ pub fn find_antinodes_in_line(antennas: &std::collections::HashMap<char, Vec<(i3
     return antinodes;
 }
 
+pub fn find_antinodes_in_line_sort_dedup(antennas: &std::collections::HashMap<char, Vec<(i32, i32)>>, antennas_map: &CharMap) -> Vec<(i32, i32)> {
+    let mut antinodes = Vec::new();
+
+    #[inline]
+    fn is_in_bounds(pos: &(i32, i32), antennas_map: &CharMap) -> bool {
+        return pos.0 >= 0 && pos.0 < antennas_map.width && pos.1 >= 0 && pos.1 < antennas_map.height;
+    }
+
+    for antenna in antennas.values() {
+        for i in 0..antenna.len() - 1 {
+            for j in i + 1..antenna.len() {
+                let antenna1 = antenna[i];
+                let antenna2 = antenna[j];
+
+                antinodes.push(antenna1);
+                antinodes.push(antenna2);
+
+                let offset = (antenna2.0 - antenna1.0, antenna2.1 - antenna1.1);
+                let count = gcd(offset.0, offset.1);
+                let offset = (offset.0 / count, offset.1 / count);
+
+                for n in 1..count {
+                    let antinode = (antenna1.0 + offset.0 * n, antenna1.1 + offset.1 * n);
+                    antinodes.push(antinode);
+                }
+
+                let mut n = 1;
+                loop {
+                    let antinode = (antenna1.0 - offset.0 * n, antenna1.1 - offset.1 * n);
+                    if !is_in_bounds(&antinode, &antennas_map) {
+                        break;
+                    }
+                    antinodes.push(antinode);
+                    n += 1;
+                }
+
+                let mut n = 1;
+                loop {
+                    let antinode = (antenna2.0 + offset.0 * n, antenna2.1 + offset.1 * n);
+                    if !is_in_bounds(&antinode, &antennas_map) {
+                        break;
+                    }
+                    antinodes.push(antinode);
+                    n += 1;
+                }
+            }
+        }
+    }
+
+    antinodes.sort();
+    antinodes.dedup();
+    return antinodes;
+}
+
+pub fn find_antinodes_in_line_hash_set(antennas: &std::collections::HashMap<char, Vec<(i32, i32)>>, antennas_map: &CharMap) -> Vec<(i32, i32)> {
+    let mut antinodes = HashSet::new();
+
+    #[inline]
+    fn is_in_bounds(pos: &(i32, i32), antennas_map: &CharMap) -> bool {
+        return pos.0 >= 0 && pos.0 < antennas_map.width && pos.1 >= 0 && pos.1 < antennas_map.height;
+    }
+
+    for antenna in antennas.values() {
+        for i in 0..antenna.len() - 1 {
+            for j in i + 1..antenna.len() {
+                let antenna1 = antenna[i];
+                let antenna2 = antenna[j];
+
+                antinodes.insert(antenna1);
+                antinodes.insert(antenna2);
+
+                let offset = (antenna2.0 - antenna1.0, antenna2.1 - antenna1.1);
+                let count = gcd(offset.0, offset.1);
+                let offset = (offset.0 / count, offset.1 / count);
+
+                for n in 1..count {
+                    let antinode = (antenna1.0 + offset.0 * n, antenna1.1 + offset.1 * n);
+                    antinodes.insert(antinode);
+                }
+
+                let mut n = 1;
+                loop {
+                    let antinode = (antenna1.0 - offset.0 * n, antenna1.1 - offset.1 * n);
+                    if !is_in_bounds(&antinode, &antennas_map) {
+                        break;
+                    }
+                    antinodes.insert(antinode);
+                    n += 1;
+                }
+
+                let mut n = 1;
+                loop {
+                    let antinode = (antenna2.0 + offset.0 * n, antenna2.1 + offset.1 * n);
+                    if !is_in_bounds(&antinode, &antennas_map) {
+                        break;
+                    }
+                    antinodes.insert(antinode);
+                    n += 1;
+                }
+            }
+        }
+    }
+
+    return antinodes.iter().copied().collect();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -161,10 +331,46 @@ mod tests {
     }
 
     #[test]
+    fn test_find_antinodes_sort_dedup() {
+        let antennas_map = CharMap::new(EXAMPLE_INPUT.to_string());
+        let antennas = gather_antennas(&antennas_map);
+        let antinodes = find_antinodes_sort_dedup(&antennas, &antennas_map);
+
+        assert_eq!(antinodes.len(), 14);
+    }
+
+    #[test]
+    fn test_find_antinodes_hash_set() {
+        let antennas_map = CharMap::new(EXAMPLE_INPUT.to_string());
+        let antennas = gather_antennas(&antennas_map);
+        let antinodes = find_antinodes_hash_set(&antennas, &antennas_map);
+
+        assert_eq!(antinodes.len(), 14);
+    }
+
+    #[test]
     fn test_find_antinodes_in_line() {
         let antennas_map = CharMap::new(EXAMPLE_INPUT.to_string());
         let antennas = gather_antennas(&antennas_map);
         let antinodes = find_antinodes_in_line(&antennas, &antennas_map);
+
+        assert_eq!(antinodes.len(), 34);
+    }
+
+    #[test]
+    fn test_find_antinodes_in_line_sort_dedup() {
+        let antennas_map = CharMap::new(EXAMPLE_INPUT.to_string());
+        let antennas = gather_antennas(&antennas_map);
+        let antinodes = find_antinodes_in_line_sort_dedup(&antennas, &antennas_map);
+
+        assert_eq!(antinodes.len(), 34);
+    }
+
+    #[test]
+    fn test_find_antinodes_in_line_hash_set() {
+        let antennas_map = CharMap::new(EXAMPLE_INPUT.to_string());
+        let antennas = gather_antennas(&antennas_map);
+        let antinodes = find_antinodes_in_line_hash_set(&antennas, &antennas_map);
 
         assert_eq!(antinodes.len(), 34);
     }
