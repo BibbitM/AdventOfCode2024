a5516120bcf6aaa0b1ec0aaf35c74b750cfcc971
Michał Nowak <bibbit@wp.pl>
2024-12-08 21:39:14 +0100

    Fix Day 06 benchmark after GuardMap -> CharMap refactor

--- benches/day_06_benchmark.rs
+++ benches/day_06_benchmark.rs
@@ -1,13 +1,14 @@
-﻿use advent::day_06;
+﻿use advent::char_map::CharMap;
+use advent::day_06;
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::fs;
 
-fn day_06_block_guard(guard_map: &day_06::GuardMap) {
+fn day_06_block_guard(guard_map: &CharMap) {
     let mut guard_map = guard_map.clone();
     _ = day_06::block_guard(&mut guard_map);
 }
 
-fn day_06_block_guard_assign_map(guard_map: &day_06::GuardMap) {
+fn day_06_block_guard_assign_map(guard_map: &CharMap) {
     let mut guard_map = guard_map.clone();
     _ = day_06::block_guard_assign_map(&mut guard_map);
 }
@@ -15,7 +16,7 @@ fn day_06_block_guard_assign_map(guard_map: &day_06::GuardMap) {
 fn day_06_benchmark(c: &mut Criterion) {
     let input = "inputs\\day_06.txt";
     let input = fs::read_to_string(input).expect("Error: Failed to read file 'inputs\\day_06.txt'");
-    let guard_map = day_06::GuardMap::new(input);
+    let guard_map = CharMap::new(input);
 
     let mut group = c.benchmark_group("day_06__block_guard");
 
