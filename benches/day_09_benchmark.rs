b1c7ae8a258e29e215b61613355af6f26596009a
Michał Nowak <bibbit@wp.pl>
2024-12-10 11:31:48 +0100

    Add benchmark for Day 09

---
+++ benches/day_09_benchmark.rs
@@ -0,0 +1,24 @@
+﻿use advent::day_09;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::fs;
+
+fn day_09_part_one_benchmark(c: &mut Criterion) {
+    let input = "inputs\\day_09.txt";
+    let input = fs::read_to_string(input).expect("Error: Failed to read file 'inputs\\day_09.txt'");
+
+    let mut group = c.benchmark_group("day_09__part_one");
+
+    group.bench_function("original", |b| b.iter(|| _ = day_09::calculate_filesystem_checksum(&input)));
+}
+
+fn day_09_part_two_benchmark_part_two(c: &mut Criterion) {
+    let input = "inputs\\day_09.txt";
+    let input = fs::read_to_string(input).expect("Error: Failed to read file 'inputs\\day_09.txt'");
+
+    let mut group = c.benchmark_group("day_09__part_two");
+
+    group.bench_function("original", |b| b.iter(|| _ = day_09::calculate_filesystem_checksum_v2(&input)));
+}
+
+criterion_group!(benches, day_09_part_one_benchmark, day_09_part_two_benchmark_part_two);
+criterion_main!(benches);
